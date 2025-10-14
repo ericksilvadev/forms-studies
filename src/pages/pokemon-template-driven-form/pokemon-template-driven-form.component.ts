7c7d5141d5df06689d35ee8d2e2e48f7173ca2b1
Erick Silva <erick.silva6653@gmail.com>
2025-10-13 21:00:03 -0300

    template driven bindings and validation

--- src/pages/pokemon-template-driven-form/pokemon-template-driven-form.component.ts
+++ src/pages/pokemon-template-driven-form/pokemon-template-driven-form.component.ts
@@ -1,10 +1,32 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, input, output, signal } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+interface PokemonForm {
+  name: string;
+  weight: number | null;
+  height: number | null;
+  imageUrl?: string;
+}
 
 @Component({
   selector: 'app-pokemon-template-driven-form',
-  imports: [],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './pokemon-template-driven-form.component.html',
-  styleUrl: './pokemon-template-driven-form.component.css',
+  styleUrls: ['./pokemon-template-driven-form.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PokemonTemplateDrivenFormComponent { }
+export class PokemonTemplateDrivenFormComponent {
+  protected model = signal<PokemonForm>({
+    name: '',
+    weight: null,
+    height: null,
+    imageUrl: '',
+  });
+
+  onSubmit(form: NgForm) {
+    if (form.invalid) return;
+
+    console.log('Form submitted', form.value);
+  }
+}
