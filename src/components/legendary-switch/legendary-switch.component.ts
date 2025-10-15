bbd190332091406fb554586dfa40e91431cd6e65
Erick Silva <erick.silva6653@gmail.com>
2025-10-14 21:40:06 -0300

    legendary switch value accessor

--- src/components/legendary-switch/legendary-switch.component.ts
+++ src/components/legendary-switch/legendary-switch.component.ts
@@ -1,15 +1,46 @@
-import { Component, forwardRef } from '@angular/core';
+import { Component, forwardRef, signal } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-legendary-switch',
   imports: [],
   templateUrl: './legendary-switch.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => LegendarySwitchComponent),
+      multi: true,
+    }
+  ]
 })
-export class LegendarySwitchComponent {
+export class LegendarySwitchComponent implements ControlValueAccessor {
+
   value = false;
+  onChange = (value: boolean) => {};
+  onTouched = () => {};
+  disabled = signal(false);
 
   toggleValue() {
+    if (this.disabled()) return;
+
     this.value = !this.value;
+    this.onChange(this.value);
+    this.onTouched();
+  }
+
+  writeValue(isLegendary: boolean): void {
+    this.value = isLegendary;
+  }
+
+  registerOnChange(fn: (value: boolean) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled.set(isDisabled);
   }
 }
--- src/pages/pokemon-template-driven-form/pokemon-template-driven-form.component.ts
+++ src/pages/pokemon-template-driven-form/pokemon-template-driven-form.component.ts
@@ -3,17 +3,19 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { ForbiddenNamesDirective } from "../../directives/forbidden-names.directive";
 import { PokemonExistsDirective } from "../../directives/pokemon-exists.directive";
+import { LegendarySwitchComponent } from '../../components/legendary-switch/legendary-switch.component';
 
 interface PokemonForm {
   name: string;
   weight: number | null;
   height: number | null;
   imageUrl?: string;
+  legendary: boolean;
 }
 
 @Component({
   selector: 'app-pokemon-template-driven-form',
-  imports: [FormsModule, JsonPipe, ForbiddenNamesDirective, PokemonExistsDirective],
+  imports: [FormsModule, JsonPipe, ForbiddenNamesDirective, PokemonExistsDirective, LegendarySwitchComponent],
   templateUrl: './pokemon-template-driven-form.component.html',
   styleUrls: ['./pokemon-template-driven-form.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -24,6 +26,7 @@ export class PokemonTemplateDrivenFormComponent {
     weight: null,
     height: null,
     imageUrl: '',
+    legendary: true,
   });
 
   onSubmit(form: NgForm) {
